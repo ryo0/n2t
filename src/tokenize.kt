455ff5b1c5d0fecb29d3aacbffc0d345e00cff01
User Name <ryo@unentai.jp>
2019-04-24 17:09:01 +0900

    予約語 letに対応

--- src/tokenize.kt
+++ src/tokenize.kt
@@ -6,8 +6,7 @@ sealed class Token {
     data class Parentheses(val par: String) : Token()
 }
 
-fun main(args: Array<String>) {
-    val inputStr = "(1 + 2) * 30 == 90"
+fun tokenize(inputStr: String): List<Token> {
     var i = 0
     val tokens = mutableListOf<Token>()
     while (i < inputStr.length) {
@@ -26,31 +25,90 @@ fun main(args: Array<String>) {
             '=' -> if (inputStr[i + 1] == '=') {
                 tokens.add(Token.Operand((inputStr[i].plus(inputStr[i + 1].toString()))))
                 i += 2
+            } else {
+                tokens.add(Token.Operand((str.toString())))
+                i++
             }
-            else ->
+            else -> {
+                val strFromIToLast = inputStr.slice(i until inputStr.length)
                 if (str.isDigit()) {
-                    val digit = get_digit(inputStr.slice(i until inputStr.length))
+                    val digit = getDigit(strFromIToLast)
                     tokens.add(Token.Number(digit))
                     i += digit.length
+                } else if (isKeyword(strFromIToLast, "let")) {
+                    val keyword = strFromIToLast.slice(0 until "let".length)
+                    tokens.add(Token.Keyword(keyword))
+                    i += "let".length
+                } else if (str.isLetter()) {
+                    val varName = getVariable(strFromIToLast)
+                    tokens.add(Token.Variable(varName))
+                    i += varName.length
                 } else {
                     i++
                 }
+            }
         }
     }
-    println(tokens.toString())
+    return tokens
 }
 
-fun get_digit(string: String): String {
+fun main(args: Array<String>) {
+    val inputStr = """
+        |let x = 2
+        |leti le
+    """.trimMargin()
+    println(tokenize(inputStr))
+}
+
+fun getVariable(string: String): String {
     var i = 0
-    var resultDigit = ""
+    var result = ""
     while (i < string.length) {
         val str = string[i]
-        if (str.isDigit()) {
-            resultDigit += str
+        if (str.isLetter() || str.isDigit()) {
+            result += str
             i++
         } else {
             break
         }
     }
-    return resultDigit
+    return result
+}
+
+fun getDigit(string: String): String {
+    var i = 0
+    var result = ""
+    while (i < string.length) {
+        val str = string[i]
+        if (str.isDigit()) {
+            result += str
+            i++
+        } else {
+            break
+        }
+    }
+    return result
+}
+
+fun isKeyword(string: String, keyword: String): Boolean {
+//    letだけ対応してみる
+    // 1. 文字数が3以上あるか？
+    // 2. 3以上あるなら、それがletと一致するか？
+    // 3. 4文字目があるなら、それは変数を構成する文字ではないか？
+
+    // letx let
+    val keywordLength = keyword.length
+
+    if (string.length < keywordLength) {
+        return false
+    }
+    val stringEqualsKeyword = string.slice(0 until keywordLength) == keyword
+    if (stringEqualsKeyword) {
+        return string.length <= keywordLength || !isAlnum(string[keywordLength])
+    }
+    return false
+}
+
+fun isAlnum(ch: Char): Boolean {
+    return ch.isLetter() || ch.isDigit()
 }
