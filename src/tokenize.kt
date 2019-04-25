fcf8c461219b6d2e9c62e9877ea512eea0cc6777
User Name <ryo@unentai.jp>
2019-04-25 17:48:58 +0900

    コメント除去

--- src/tokenize.kt
+++ src/tokenize.kt
@@ -1,7 +1,7 @@
 sealed class Token {
     data class Number(val num: String) : Token()
     data class Keyword(val key: String) : Token()
-    data class VarOrKeyword(val name: String): Token()
+    data class VarOrKeyword(val name: String) : Token()
     data class Variable(val varName: String) : Token()
     data class Operand(val op: String) : Token()
     data class Parentheses(val par: String) : Token()
@@ -55,12 +55,12 @@ fun isKeyword(string: String): Boolean {
 }
 
 fun filterVarOrKeyword(tokens: List<Token>): List<Token> {
-    return tokens.map{
-        if(it is Token.VarOrKeyword){
+    return tokens.map {
+        if (it is Token.VarOrKeyword) {
             if (isKeyword(it.name)) {
                 Token.Keyword(it.name)
             } else {
-               Token.Variable(it.name)
+                Token.Variable(it.name)
             }
         } else {
             it
@@ -75,7 +75,48 @@ fun main(args: Array<String>) {
         |(3 + 2 * let) = 3
     """.trimMargin()
     println(filterVarOrKeyword(tokenize(inputStr)))
+
+    val x = """
+    	// ねこ
+        // かに
+        let x = 2
+        /* aaa */ ebi = 5 /* nekoneko */ * 2 + 2
+        /* 111 */ b //a
+        |let3 le class
+        |(3 + 2 * let) = 3
+    """
+    println(filterVarOrKeyword(tokenize(removeComments(x))))
 }
+
+fun removeComments(string: String): String {
+    var nowInOneLineComment = false
+    var nowInRangeComment = false
+    var result = ""
+    var i = 0
+    while (i < string.length - 1) {
+        val str1 = string[i]
+        val str2 = string[i + 1]
+        if (str1 == '/' && str2 == '/') {
+            nowInOneLineComment = true
+        } else if (str1 == '/' && str2 == '*') {
+            nowInRangeComment = true
+        } else if (nowInOneLineComment) {
+            if (str1 == '\n') {
+                nowInOneLineComment = false
+            }
+        } else if (nowInRangeComment) {
+            if (str1 == '*' && str2 == '/') {
+                nowInRangeComment = false
+                i++ // 二文字分スキップしたいので余計に一つ進めておく
+            }
+        } else {
+            result += str1
+        }
+        i++
+    }
+    return result
+}
+
 
 fun getVariable(string: String): String {
     var i = 0
